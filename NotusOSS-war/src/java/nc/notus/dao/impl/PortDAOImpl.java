c0b76547ccef35d0a2b23de90e76f9c0242c90c0
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-13 17:26:30 +0300

    Return list of all our free ports in system for NC.KYIV.2014.WIND.SOW.3 requirement

--- NotusOSS-war/src/java/nc/notus/dao/impl/PortDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/PortDAOImpl.java
@@ -1,11 +1,37 @@
 package nc.notus.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
 import nc.notus.dao.PortDAO;
+import nc.notus.dbmanager.ResultIterator;
+import nc.notus.dbmanager.Statement;
 import nc.notus.entity.Port;
 
 /**
  * Implementation of DAO for entity Port
- * @author Andrey Ilin
+ * @author Vladimir Ermolenko
  */
 public class PortDAOImpl extends GenericDAOImpl<Port> implements PortDAO {
+
+    /**
+     * Return list of all our free ports in system
+     * @return list of all free ports
+     */
+    public List<Port> getFreePort() {
+        List<Port> fp = new ArrayList<Port>();
+        Port  port = null;
+        String query = "SELECT p.id, p.deviceID, p.portNumber, p.portStatus, p.cableID" +
+                       "FROM port p WHERE p.portStatus = 0";// portStatus  - it's a flag with 0 - free and with 1  - connected
+        Statement statement = dbManager.prepareStatement(query);
+        ResultIterator ri = statement.executeQuery();
+        while (ri.next()) {
+            port.setId(ri.getInt("id"));
+            port.setDeviceID(ri.getInt("deviceID"));
+            port.setPortNumber(ri.getInt("portNumber"));
+            port.setPortStatus(ri.getInt("portStatus"));
+            port.setCableID(ri.getInt("cableID"));
+            fp.add(port);
+        }
+        return fp;
+    }
 }
