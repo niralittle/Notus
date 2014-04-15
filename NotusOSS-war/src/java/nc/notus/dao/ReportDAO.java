4e5c0f11df23939c436e7821be807e7b33a9d9a6
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-15 19:11:25 +0300

    just started ReportDAO & ReportDAOImpl for our reports

---
+++ NotusOSS-war/src/java/nc/notus/dao/ReportDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nc.notus.dao;
+
+/**
+ * Interface of DAO for our reports
+ * @author Vladimir Ermolenko
+ */
+public interface ReportDAO {
+
+}
---
+++ NotusOSS-war/src/java/nc/notus/dao/impl/ReportDAOImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nc.notus.dao.impl;
+
+import nc.notus.dao.ReportDAO;
+
+/**
+ * Implementation of DAO for our reports
+ * @author Vladimir Ermolenko
+ */
+public class ReportDAOImpl implements ReportDAO {
+    
+}
