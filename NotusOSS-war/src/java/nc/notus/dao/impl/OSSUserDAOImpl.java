71fdf5bfe7df92cbe21e88914ccf65874a3e4112
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-04-17 00:08:12 +0300

    Add methods implementation

--- NotusOSS-war/src/java/nc/notus/dao/impl/OSSUserDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/OSSUserDAOImpl.java
@@ -29,16 +29,49 @@ public class OSSUserDAOImpl extends GenericDAOImpl<OSSUser> implements OSSUserDA
         statement.executeUpdate();
     }
     
-    @Override
-    public boolean isExist(String login) {
-    	if (login == null) {
+    
+    /**
+     * Implementation of checking if user with specified login is already exist in system
+     * 
+     * @param login login to check
+     * @return true - if exist, false - otherwise
+     * @throws NullPointerException if null reference passes
+     */
+	@Override
+	public boolean isLoginDuplicate(String login) {
+		if (login == null) {
     		throw new NullPointerException("Null reference invoke.");
     	}
-    	String queryString = "SELECT lastname FROM OSSUSER WHERE login = ?";
+    	String queryString = "SELECT login FROM OSSUSER WHERE login = ?";
 	    Statement statement = dbManager.prepareStatement(queryString);
 	    statement.setString(1, login);
 	    ResultIterator ri = statement.executeQuery();
-	    return ri != null ? true : false;
+	    if ( ri.next()) {
+	    	return true;
+	    }
+	    return false;
+	}
 
-    }
+
+     /**
+     * Implementation of checking if specified email is already exist in system
+     * 
+     * @param email email to check
+     * @return true - if exist, false - otherwise
+     * @throws NullPointerException if null reference passes
+     */
+	@Override
+	public boolean isEmailDuplicate(String email) {
+		if (email == null) {
+    		throw new NullPointerException("Null reference invoke.");
+    	}
+    	String queryString = "SELECT email FROM OSSUSER WHERE email = ?";
+	    Statement statement = dbManager.prepareStatement(queryString);
+	    statement.setString(1, email);
+	    ResultIterator ri = statement.executeQuery();
+	    if ( ri.next()) {
+	    	return true;
+	    }
+	    return false;
+	}
 }
