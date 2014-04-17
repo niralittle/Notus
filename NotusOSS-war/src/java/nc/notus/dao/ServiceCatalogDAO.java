d779ef5d2df20fc41200e8fab197dc8440d177c8
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-17 17:41:54 +0300

    just started method getServiceCatalogByProviderLocationID

--- NotusOSS-war/src/java/nc/notus/dao/ServiceCatalogDAO.java
+++ NotusOSS-war/src/java/nc/notus/dao/ServiceCatalogDAO.java
@@ -4,7 +4,14 @@ import nc.notus.entity.ServiceCatalog;
 
 /**
  * Interface of DAO for entity ServiceCatalog.
- * @author Andrey Ilin
+ * @author Vladimir Ermolenko
  */
 public interface ServiceCatalogDAO extends GenericDAO<ServiceCatalog> {
+
+    /**
+     * Method to obtain services and prices(ServicCatalog) by providerLocationID
+     * @param id
+     * @return ServiceCatalog object
+     */
+    public ServiceCatalog getServiceCatalogByProviderLocationID(int id);
 }
--- NotusOSS-war/src/java/nc/notus/dao/impl/ServiceCatalogDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/ServiceCatalogDAOImpl.java
@@ -6,13 +6,23 @@ import nc.notus.entity.ServiceCatalog;
 
 /**
  * Implementation of DAO for entity ServiceCatalog
- * @author Andrey Ilin
+ * @author Vladimir Ermolenko
  */
 public class ServiceCatalogDAOImpl extends GenericDAOImpl<ServiceCatalog>
         implements ServiceCatalogDAO {
 
     public ServiceCatalogDAOImpl(DBManager dbManager) {
         super(dbManager);
+    }
+
+    /**
+     * Method to obtain services and prices(ServicCatalog) by providerLocationID
+     * @param id
+     * @return ServiceCatalog object
+     */
+    @Override
+    public ServiceCatalog getServiceCatalogByProviderLocationID(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }
