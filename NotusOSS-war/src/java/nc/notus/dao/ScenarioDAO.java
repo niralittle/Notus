c8164ff3e752bb9f7dfbf9f641ed9855cf3ebe2c
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-17 19:52:44 +0300

    added missing DAO interfaces

---
+++ NotusOSS-war/src/java/nc/notus/dao/ScenarioDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nc.notus.dao;
+
+import nc.notus.entity.Scenario;
+
+/**
+ * Interface of DAO for entity Scenario
+ * @author Vladimir Ermolenko
+ */
+public interface ScenarioDAO extends GenericDAO<Scenario> {
+
+}
---
+++ NotusOSS-war/src/java/nc/notus/dao/ServiceTypeDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nc.notus.dao;
+
+import nc.notus.entity.ServiceType;
+
+/**
+ * Interface of DAO for entity ServiceType
+ * @author Vladimir Ermolenko
+ */
+public interface ServiceTypeDAO extends GenericDAO<ServiceType>{
+
+}
