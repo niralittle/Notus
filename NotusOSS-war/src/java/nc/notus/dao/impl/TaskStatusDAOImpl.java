1a485f0d645265a84cea4acae1d2e0691333e4ff
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-17 19:37:09 +0300

    finished method getTaskStatusID(TaskState taskState)

--- NotusOSS-war/src/java/nc/notus/dao/impl/TaskStatusDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/TaskStatusDAOImpl.java
@@ -7,6 +7,8 @@ package nc.notus.dao.impl;
 
 import nc.notus.dao.TaskStatusDAO;
 import nc.notus.dbmanager.DBManager;
+import nc.notus.dbmanager.ResultIterator;
+import nc.notus.dbmanager.Statement;
 import nc.notus.entity.TaskStatus;
 import nc.notus.states.TaskState;
 
@@ -25,7 +27,16 @@ public class TaskStatusDAOImpl extends GenericDAOImpl<TaskStatus> implements Tas
      * @return id of task
      */
     public int getTaskStatusID(TaskState taskState) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int  id = 0;
+        String query = "SELECT ts.id, ts.status" +
+                       "FROM taskstatus ts WHERE ts.status = ?";
+        Statement statement = dbManager.prepareStatement(query);
+        statement.setString(1, taskState.toString());
+        ResultIterator ri = statement.executeQuery();
+        if (ri.next()) {
+            return ri.getInt("id");
+        }
+        return id;
     }
 
 }
