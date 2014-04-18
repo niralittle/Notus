9bd4dfea7051cfaf97ef4f6d67fa975e0a8b3fb0
Igor <zzzeeerrr0@gmail.com>
2014-04-18 15:20:23 +0300

    Added task management and RI management to Workflow

--- NotusOSS-war/src/java/nc/notus/workflow/Workflow.java
+++ NotusOSS-war/src/java/nc/notus/workflow/Workflow.java
@@ -1,28 +1,35 @@
 package nc.notus.workflow;
 
 import nc.notus.dao.OSSUserDAO;
+import nc.notus.dao.ScenarioDAO;
+import nc.notus.dao.ServiceOrderDAO;
 import nc.notus.dao.ServiceOrderStatusDAO;
 import nc.notus.dao.TaskDAO;
 import nc.notus.dao.TaskStatusDAO;
 import nc.notus.dao.impl.OSSUserDAOImpl;
+import nc.notus.dao.impl.ScenarioDAOImpl;
+import nc.notus.dao.impl.ServiceOrderDAOImpl;
 import nc.notus.dao.impl.ServiceOrderStatusDAOImpl;
 import nc.notus.dao.impl.TaskDAOImpl;
 import nc.notus.dao.impl.TaskStatusDAOImpl;
 import nc.notus.dbmanager.DBManager;
 import nc.notus.entity.OSSUser;
+import nc.notus.entity.Scenario;
 import nc.notus.entity.ServiceOrder;
+import nc.notus.entity.ServiceOrderStatus;
 import nc.notus.entity.Task;
 import nc.notus.states.OrderStatus;
 import nc.notus.states.TaskState;
+import nc.notus.states.UserRole;
 
 /**
- * This class provides functionality of managing workflow under particular Order.
- * All task management focused here.
+ * This class provides functionality of managing workflow for particular Order.
+ * All task and order management focused here.
  * @author Igor Litvinenko
  */
 public abstract class Workflow {
 
-    protected ServiceOrder order; // Service Order under which workflow was created
+    protected ServiceOrder order; // Service Order for which workflow was created
 
     public Workflow(ServiceOrder order) {
         this.order = order;
@@ -34,19 +41,23 @@ public abstract class Workflow {
      */
     public abstract void proceedOrder();
 
-    protected boolean isNewOrder() {
-        DBManager dbManager = new DBManager();
-        ServiceOrderStatusDAO sosDAO = new ServiceOrderStatusDAOImpl(dbManager);
-        boolean isNewOrder;
-        if (order.getServiceOrderStatusID() ==
-                    sosDAO.getServiceOrderStatusID(OrderStatus.ENTERING)) {
-            isNewOrder = true;
-        } else {
-            isNewOrder = false;
-        }
-        dbManager.close();
+    /**
+     * This method is used to create tasks and assign it to user groups.
+     * Method is <code>protected</code> because it can only be invoked in
+     * method of particular Workflow.
+     * @param dbManager connection to database encapsulated in DBManager class
+     * @param userRole identifies user group to create task for
+     */
+    protected void createTask(DBManager dbManager, UserRole userRole) {
+        TaskDAO taskDAO = new TaskDAOImpl(dbManager);
+        TaskStatusDAO taskStatusDAO = new TaskStatusDAOImpl(dbManager);
 
-        return isNewOrder;
+        Task task = new Task();
+        task.setEmployeeID(null);
+        task.setRoleID(userRole.toInt());
+        task.setServiceOrderID(order.getId());
+        task.setTaskStatusID(taskStatusDAO.getTaskStatusID(TaskState.ACTIVE));
+        taskDAO.add(task);
     }
 
     /**
@@ -91,5 +102,28 @@ public abstract class Workflow {
 
         dbManager.commit();
         dbManager.close();
+    }
+
+    protected String getOrderStatus(DBManager dbManager) {
+        ServiceOrderStatusDAO orderStatusDAO = new ServiceOrderStatusDAOImpl(dbManager);
+        int statusID = order.getServiceOrderStatusID();
+        ServiceOrderStatus status = orderStatusDAO.find(statusID);
+        return status.getStatus();
+    }
+
+    protected String getOrderScenario(DBManager dbManager) {
+        ScenarioDAO scenarioDAO = new ScenarioDAOImpl(dbManager);
+        int scenarioID = order.getScenarioID();
+        Scenario scenario = scenarioDAO.find(scenarioID);
+        return scenario.getScenario();
+    }
+
+    protected void changeOrderStatus(DBManager dbManager, OrderStatus status) {
+        ServiceOrderDAO orderDAO = new ServiceOrderDAOImpl(dbManager);
+        ServiceOrderStatusDAO orderStatusDAO = new ServiceOrderStatusDAOImpl(dbManager);
+
+        int statusID = orderStatusDAO.getServiceOrderStatusID(status);
+        order.setServiceOrderStatusID(statusID);
+        orderDAO.update(order);
     }
 }
