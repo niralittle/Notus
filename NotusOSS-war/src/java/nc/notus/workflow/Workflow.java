b197b2a599a6511325723753d98aca81414d4c75
Igor <zzzeeerrr0@gmail.com>
2014-04-18 13:06:16 +0300

    Added documentation to general Workflow class

--- NotusOSS-war/src/java/nc/notus/workflow/Workflow.java
+++ NotusOSS-war/src/java/nc/notus/workflow/Workflow.java
@@ -3,11 +3,9 @@ package nc.notus.workflow;
 import nc.notus.dao.OSSUserDAO;
 import nc.notus.dao.ServiceOrderStatusDAO;
 import nc.notus.dao.TaskDAO;
-import nc.notus.dao.TaskStatusDAO;
 import nc.notus.dao.impl.OSSUserDAOImpl;
 import nc.notus.dao.impl.ServiceOrderStatusDAOImpl;
 import nc.notus.dao.impl.TaskDAOImpl;
-import nc.notus.dao.impl.TaskStatusDAOImpl;
 import nc.notus.dbmanager.DBManager;
 import nc.notus.entity.OSSUser;
 import nc.notus.entity.ServiceOrder;
@@ -15,18 +13,22 @@ import nc.notus.entity.Task;
 import nc.notus.states.OrderStatus;
 
 /**
- * TODO: documenting. It provides functionality
- * to proceed Orders through workflows.
+ * This class provides functionality of managing workflow under particular Order.
+ * All task management focused here.
  * @author Igor Litvinenko
  */
 public abstract class Workflow {
 
-    protected ServiceOrder order;
+    protected ServiceOrder order; // Service Order under which workflow was created
 
     public Workflow(ServiceOrder order) {
         this.order = order;
     }
 
+    /**
+     * This method proceeds Order by creating tasks for
+     * corresponding user groups which take part in Order execution
+     */
     public abstract void proceedOrder();
 
     protected boolean isNewOrder() {
@@ -44,6 +46,12 @@ public abstract class Workflow {
         return isNewOrder;
     }
 
+    /**
+     * This method assigns task to particular user of user group
+     * responsible for task execution
+     * @param taskID ID of task to assign user to
+     * @param userID ID of user to assign
+     */
     public void assignTask(int taskID, int userID) {
         DBManager dbManager = new DBManager();
         TaskDAO taskDAO = new TaskDAOImpl(dbManager);
@@ -64,6 +72,10 @@ public abstract class Workflow {
         }
     }
 
+    /**
+     * This method sets task status to "Completed"
+     * @param taskID ID of task
+     */
     public void completeTask(int taskID) {
         /*DBManager dbManager = new DBManager();
         TaskDAO taskDAO = new TaskDAOImpl(dbManager);
