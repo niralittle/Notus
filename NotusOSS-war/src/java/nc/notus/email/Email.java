872ee805629e2addf37b9847ce550e0212985895
Igor <zzzeeerrr0@gmail.com>
2014-04-22 22:28:51 +0300

    Added Email interface for email templates

---
+++ NotusOSS-war/src/java/nc/notus/email/Email.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nc.notus.email;
+
+/**
+ *
+ * @author Igor Litvinenko
+ */
+public interface Email {
+
+    String getMessage();
+
+    String getTitle();
+}
