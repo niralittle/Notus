0f4adbc84c502db4436724339df3043daea708f1
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-22 23:16:37 +0300

    added method public List<String> getUniqueTypeFreeCables()

--- NotusOSS-war/src/java/nc/notus/dao/CableDAO.java
+++ NotusOSS-war/src/java/nc/notus/dao/CableDAO.java
@@ -1,11 +1,17 @@
 package nc.notus.dao;
 
+import java.util.List;
 import nc.notus.entity.Cable;
 
 /**
  * Interface of DAO for entity Cable
- * @author Igor Litvinenko
+ * @author Igor Litvinenko & Vladimir Ermolenko
  */
 public interface CableDAO extends GenericDAO<Cable> {
 
+    /**
+     * Method returns a list of unique type(names) of unused in Port cables
+     * @return list of unique type(names) of unused in Port cables
+     */
+    public List<String> getUniqueTypeFreeCables();
 }
--- NotusOSS-war/src/java/nc/notus/dao/impl/CableDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/CableDAOImpl.java
@@ -1,17 +1,42 @@
 package nc.notus.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
 import nc.notus.dao.CableDAO;
 import nc.notus.dbmanager.DBManager;
+import nc.notus.dbmanager.ResultIterator;
+import nc.notus.dbmanager.Statement;
 import nc.notus.entity.Cable;
 
 /**
  * Implementation of DAO for entity Cable
- * @author Igor Litvinenko
+ * @author Igor Litvinenko & Vladimir Ermolenko
  */
 public class CableDAOImpl extends GenericDAOImpl<Cable> implements CableDAO {
 
     public CableDAOImpl(DBManager dbManager) {
         super(dbManager);
+    }
+
+    /**
+     * Method returns a list of unique type(names) of unused in Port cables
+     * @return list of unique type(names) of unused in Port cables
+     */
+    @Override
+    public List<String> getUniqueTypeFreeCables() {
+        
+        String query  = "SELECT c.cable " +
+                        "FROM cable c " +
+                        "LEFT JOIN port p ON c.id = p.cableid " +
+                        "WHERE p.cableid IS NULL " +
+                        "GROUP BY c.cable";
+        Statement statement = dbManager.prepareStatement(query);
+        ResultIterator ri = statement.executeQuery();
+        List<String> freeCables = new ArrayList<String>();
+        while (ri.next()){
+            freeCables.add(ri.getString("cable"));
+        }
+        return freeCables;
     }
 
 }
