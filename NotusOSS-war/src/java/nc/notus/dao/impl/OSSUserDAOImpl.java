c9ed2ff17b60a5a5cb8fa7c9aff314a5a9aa62b4
Vladimir Ermolenko <vlader2@yahoo.com>
2014-04-24 11:50:58 +0300

    added method getUsersByEmail

--- NotusOSS-war/src/java/nc/notus/dao/impl/OSSUserDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/OSSUserDAOImpl.java
@@ -151,7 +151,41 @@ public class OSSUserDAOImpl extends GenericDAOImpl<OSSUser> implements OSSUserDA
         return emailList;
     }
 
+    /**
+     * Method returns list of users with similar email with paging
+     * @param offset - offset from start position in paging
+     * @param numberOfRecords - quantity of records to fetch
+     * @return list of users with similar email
+     */
     public List<OSSUser> getUsersByEmail(String email, int offset, int numberOfRecords) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
+                        "SELECT u.id, u.firstname, u.lastname, u.email, u.login, " +
+                        "u.password, u.blocked, u.roleid " +
+                        "FROM ossuser u " +
+                        "LEFT JOIN role r ON u.roleid = r.id " +
+                        "WHERE r.role  = 'Customer User' " +
+                        "AND u.email LIKE ? " +
+                        "ORDER BY u.firstname " +
+                        " ) a where ROWNUM <= ? )" +
+                        "WHERE rnum  >= ?";
+        Statement statement = dbManager.prepareStatement(query);
+        statement.setString(1, email);
+        statement.setInt(2, numberOfRecords);
+        statement.setInt(3, offset);
+        ResultIterator ri = statement.executeQuery();
+        List<OSSUser> users = new ArrayList<OSSUser>();
+        while (ri.next()){
+            OSSUser us = new OSSUser();
+            us.setId(ri.getInt("id"));
+            us.setFirstName(ri.getString("firstname"));
+            us.setLastName(ri.getString("lastname"));
+            us.setEmail(ri.getString("email"));
+            us.setLogin(ri.getString("login"));
+            us.setPassword(ri.getString("password"));
+            us.setBlocked(ri.getInt("blocked"));
+            us.setRoleID(ri.getInt("roleid"));
+            users.add(us);
+        }
+        return users;
     }
 }
