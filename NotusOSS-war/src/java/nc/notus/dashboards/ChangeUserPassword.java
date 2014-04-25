6e4c401729d62cb55df84350400c2ff7b36d95aa
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-04-25 19:31:17 +0300

    add changing user password servlet

--- NotusOSS-war/src/java/nc/notus/dashboards/ChangeUserPassword.java
+++ NotusOSS-war/src/java/nc/notus/dashboards/ChangeUserPassword.java
@@ -1,84 +1,114 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package nc.notus.dashboards;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import nc.notus.dao.impl.OSSUserDAOImpl;
+import nc.notus.dbmanager.DBManager;
+import nc.notus.entity.OSSUser;
+
 /**
- *
- * @author Vladimir Ermolenko
+ * Change password for specified user.
+ * 
+ * @author Panchenko Dmytro
  */
 public class ChangeUserPassword extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ChangeUserPassword</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ChangeUserPassword at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            */
-        } finally { 
-            out.close();
-        }
-    } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+	private static final long serialVersionUID = 1L;
+	
+	//page to redirect
+	private static final String CHANGE_PASSWORD_PAGE = "passwordChanging.jsp";
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+		//read necessary parameters from request scope
+		String newPassword = request.getParameter("newPassword");
+		int userId = Integer.parseInt(request.getParameter("userId"));
+		
+		//create DAO
+		OSSUserDAOImpl userDAO = null;
+		DBManager dbManager = null;
+		try {
+			dbManager = new DBManager();
+			userDAO = new OSSUserDAOImpl(dbManager);
+			
+			//get user by id and set him new password
+			OSSUser user = userDAO.find(userId);
+			user.setPassword(newPassword);
+			
+			//update password in DB
+			userDAO.update(user);
+			
+			//commit changes
+			dbManager.commit();
+			
+			//say all ok
+			request.setAttribute("success", "Password changed successfully!");
+		} finally {
+			dbManager.close();
+		}
+		
+		redirect(request, response, CHANGE_PASSWORD_PAGE);
+	}
+	/**
+	 * Redirect to passes page.
+	 * 
+	 * @param request
+	 * @param response
+	 * @param page
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void redirect(HttpServletRequest request,
+			HttpServletResponse response, String page) throws ServletException,
+			IOException {
+		RequestDispatcher view = request.getRequestDispatcher(page);
+		view.forward(request, response);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 * 
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 * 
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Registers user in the system, creates a new order "
+				+ "and executes it ('New' scenario workflow).";
+	}// </editor-fold>
 
 }
