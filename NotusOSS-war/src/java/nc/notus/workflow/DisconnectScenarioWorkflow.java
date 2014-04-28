3356582ca6522b1b3c60a8775ec3bdddfbc57d33
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-04-28 15:59:20 +0300

    update logic for Provisioning engineer

--- NotusOSS-war/src/java/nc/notus/workflow/DisconnectScenarioWorkflow.java
+++ NotusOSS-war/src/java/nc/notus/workflow/DisconnectScenarioWorkflow.java
@@ -8,6 +8,7 @@ import nc.notus.dao.impl.CableDAOImpl;
 import nc.notus.dao.impl.CircuitDAOImpl;
 import nc.notus.dao.impl.PortDAOImpl;
 import nc.notus.dao.impl.ServiceInstanceDAOImpl;
+import nc.notus.dao.impl.ServiceOrderDAOImpl;
 import nc.notus.dbmanager.DBManager;
 import nc.notus.entity.Port;
 import nc.notus.entity.ServiceInstance;
@@ -20,119 +21,151 @@ import nc.notus.states.WorkflowScenario;
 
 /**
  * This class provides functionality for "Disconnect" scenario workflow
- * @author Igor Litvinenko
+ * 
+ * @author Igor Litvinenko & Panchenko Dmytro
  */
 public class DisconnectScenarioWorkflow extends Workflow {
 
-    /**
-     * This method creates DisconnectScenarioWorkflow for given Order.
-     * It doesn't proceed Order to execution(See {@link Workflow#proceedOrder()})
-     * @param order Order to create Workflow for
-     * @throws Workflow exception if Order scenario doesn't match "Disconnect"
-     * scenario workflow
-     */
-    public DisconnectScenarioWorkflow(ServiceOrder order) {
-        super(order);
-        DBManager dbManager = new DBManager();
-        try {
-            if (!getOrderScenario(dbManager).equals(WorkflowScenario.DISCONNECT.toString())) {
-                throw new WorkflowException("Cannot proceed Order: wrong order scenario");
-            }
-        } finally {
-            dbManager.close();
-        }
-    }
+	/**
+	 * This method creates DisconnectScenarioWorkflow for given Order. It
+	 * doesn't proceed Order to execution(See {@link Workflow#proceedOrder()})
+	 * 
+	 * @param order
+	 *            Order to create Workflow for
+	 * @throws Workflow
+	 *             exception if Order scenario doesn't match "Disconnect"
+	 *             scenario workflow
+	 */
+	public DisconnectScenarioWorkflow(ServiceOrder order) {
+		super(order);
+		DBManager dbManager = new DBManager();
+		try {
+			if (!getOrderScenario(dbManager).equals(
+					WorkflowScenario.DISCONNECT.toString())) {
+				throw new WorkflowException(
+						"Cannot proceed Order: wrong order scenario");
+			}
+		} finally {
+			dbManager.close();
+		}
+	}
 
-    /**
-     * This method proceeds Order by creating tasks for
-     * corresponding user groups which take part in Order execution.
-     * Order should have status "Entering" and workflow scenario "Disconnect"
-     */
-    @Override
-    public void proceedOrder() {
-        DBManager dbManager = new DBManager();
-        try {
-            if (!getOrderStatus(dbManager).equals(OrderStatus.ENTERING.toString())) {
-                throw new WorkflowException("Cannot proceed Order: wrong order state");
-            }
+	/**
+	 * This method proceeds Order by creating tasks for corresponding user
+	 * groups which take part in Order execution. Order should have status
+	 * "Entering" and workflow scenario "Disconnect"
+	 */
+	@Override
+	public void proceedOrder() {
+		DBManager dbManager = new DBManager();
+		try {
+			if (!getOrderStatus(dbManager).equals(
+					OrderStatus.ENTERING.toString())) {
+				throw new WorkflowException(
+						"Cannot proceed Order: wrong order state");
+			}
 
-            changeOrderStatus(dbManager, OrderStatus.PROCESSING);
-            createTask(dbManager, UserRole.PROVISION_ENGINEER);
-            dbManager.commit();
-        } finally {
-            dbManager.close();
-        }
-    }
+			changeOrderStatus(dbManager, OrderStatus.PROCESSING);
+			createTask(dbManager, UserRole.PROVISION_ENGINEER);
+			dbManager.commit();
+		} finally {
+			dbManager.close();
+		}
+	}
 
-    /**
-     * This method unassigns given Port from given Service Instance.
-     * It also unlinks Circuit from SI and deletes the Circuit.
-     * It sets status of given task to "Completed".
-     * After execution it automatically creates task for Installation Engineer group.
-     * @param taskID ID of task for Provisioning Engineer
-     * @param portID ID of port to unlink SI from
-     * @param serviceInstanceID ID of SI to unlink from port
-     */
-    public void unassignPortFromSI(int taskID, int portID, int serviceInstanceID) {
-        if (order.getServiceInstanceID() != serviceInstanceID) {
-            throw new WorkflowException("Given SI is not connected " +
-                    "with current Order");
-        }
+	/*
+	/**
+	 * This method unassigns given Port from given Service Instance. It also
+	 * unlinks Circuit from SI and deletes the Circuit. It sets status of given
+	 * task to "Completed". After execution it automatically creates task for
+	 * Installation Engineer group.
+	 * 
+	 * @param taskID
+	 *            ID of task for Provisioning Engineer
+	 * @param portID
+	 *            ID of port to unlink SI from
+	 * @param serviceInstanceID
+	 *            ID of SI to unlink from port
+	 */
+	public void unassignPortFromSI(int taskID, int portID, int serviceInstanceID) {
+		if (order.getServiceInstanceID() != serviceInstanceID) {
+			throw new WorkflowException("Given SI is not connected "
+					+ "with current Order");
+		}
 
-        DBManager dbManager = new DBManager();
-        try {
-            if (!isTaskValid(dbManager, taskID,
-                    UserRole.PROVISION_ENGINEER.toInt())) {
-                throw new WorkflowException("Given Task is not valid");
-            }
-            ServiceInstanceDAO siDAO = new ServiceInstanceDAOImpl(dbManager);
-            CircuitDAO circuitDAO = new CircuitDAOImpl(dbManager);
+		DBManager dbManager = new DBManager();
+		try {
+			if (!isTaskValid(dbManager, taskID,
+					UserRole.INSTALLATION_ENGINEER.toInt())) {
+				throw new WorkflowException("Given Task is not valid");
+			}
+			ServiceInstanceDAO siDAO = new ServiceInstanceDAOImpl(dbManager);
 
-            ServiceInstance si = siDAO.find(serviceInstanceID);
-            int circuitID = si.getCircuitID();
-            si.setCircuitID(null);
-            si.setPortID(null);
-            siDAO.update(si);
+			ServiceInstance si = siDAO.find(serviceInstanceID);
+			si.setPortID(null);
+			siDAO.update(si);
 
-            circuitDAO.delete(circuitID);
+			changeServiceInstanceStatus(dbManager, InstanceStatus.DISCONNECTED);
+			dbManager.commit();
+		} finally {
+			dbManager.close();
+		}
+	}
 
-            this.completeTask(dbManager, taskID);
-            changeServiceInstanceStatus(dbManager, InstanceStatus.DISCONNECTED);
-            this.createTask(dbManager, UserRole.INSTALLATION_ENGINEER);
-            dbManager.commit();
-        } finally {
-            dbManager.close();
-        }
-    }
+	/**
+	 * This method unplugs Cable from specified Port. It sets Port status to
+	 * "Free" and changes status of Task to "Completed" after execution.
+	 * 
+	 * @param taskID
+	 *            taskID ID of task for installation engineer
+	 * @param cableID
+	 *            ID of Cable to unplug
+	 * @param portID
+	 *            ID of Port to unplug Cable from
+	 */
+	public void unplugCableFromPort(int taskID, int cableID, int portID) {
+		DBManager dbManager = new DBManager();
+		try {
+			if (!isTaskValid(dbManager, taskID,
+					UserRole.INSTALLATION_ENGINEER.toInt())) {
+				throw new WorkflowException("Given Task is not valid");
+			}
 
-    /**
-     * This method unplugs Cable from specified Port.
-     * It sets Port status to "Free" and changes status of Task
-     * to "Completed" after execution.
-     * @param taskID taskID ID of task for installation engineer
-     * @param cableID ID of Cable to unplug
-     * @param portID ID of Port to unplug Cable from
-     */
-    public void unplugCableFromPort(int taskID, int cableID, int portID) {
-        DBManager dbManager = new DBManager();
-        try {
-            if (!isTaskValid(dbManager, taskID,
-                    UserRole.INSTALLATION_ENGINEER.toInt())) {
-                throw new WorkflowException("Given Task is not valid");
-            }
+			PortDAO portDAO = new PortDAOImpl(dbManager);
+			Port port = portDAO.find(portID);
+			port.setCableID(null);
+			port.setPortStatus(PortState.FREE.toInt());
+			portDAO.update(port);
 
-            PortDAO portDAO = new PortDAOImpl(dbManager);
-            Port port = portDAO.find(portID);
-            port.setCableID(null);
-            port.setPortStatus(PortState.FREE.toInt());
-            portDAO.update(port);
+			completeTask(dbManager, taskID);
+			changeOrderStatus(dbManager, OrderStatus.COMPLETED);
+			// TODO: send email here
+			dbManager.commit();
+		} finally {
+			dbManager.close();
+		}
+	}
 
-            completeTask(dbManager, taskID);
-            changeOrderStatus(dbManager, OrderStatus.COMPLETED);
-            // TODO: send email here
-            dbManager.commit();
-        } finally {
-            dbManager.close();
-        }
-    }
+	public void removeCurcuitFromSI(int taskID) {
+		DBManager dbManager = new DBManager();
+		try {
+			if (!isTaskValid(dbManager, taskID,
+					UserRole.SUPPORT_ENGINEER.toInt())) {
+				throw new WorkflowException("Given Task is not valid");
+			}
+			ServiceInstanceDAOImpl siDAO = new ServiceInstanceDAOImpl(dbManager);
+			
+			ServiceInstance si = siDAO.find(order.getServiceInstanceID());
+			si.setCircuitID(null);
+			siDAO.update(si);
+			
+			completeTask(dbManager, taskID);
+			createTask(dbManager, UserRole.INSTALLATION_ENGINEER);
+			dbManager.commit();
+			
+		} finally {
+			dbManager.close();
+
+		}
+	}
 }
