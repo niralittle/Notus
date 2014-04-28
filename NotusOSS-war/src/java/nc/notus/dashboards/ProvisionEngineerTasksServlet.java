2b3fbc14954c96ec4c93ba39d1b60b7a46b1715a
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-04-29 00:31:40 +0300

    sorting task for new and disconnect scenarios

--- NotusOSS-war/src/java/nc/notus/dashboards/ProvisionEngineerTasksServlet.java
+++ NotusOSS-war/src/java/nc/notus/dashboards/ProvisionEngineerTasksServlet.java
@@ -6,24 +6,38 @@
 package nc.notus.dashboards;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import nc.notus.dao.OSSUserDAO;
+import nc.notus.dao.ScenarioDAO;
 import nc.notus.dao.TaskDAO;
 import nc.notus.dao.impl.OSSUserDAOImpl;
+import nc.notus.dao.impl.ScenarioDAOImpl;
+import nc.notus.dao.impl.ServiceOrderDAOImpl;
 import nc.notus.dao.impl.TaskDAOImpl;
 import nc.notus.dbmanager.DBManager;
+import nc.notus.entity.Scenario;
+import nc.notus.entity.ServiceOrder;
 import nc.notus.entity.Task;
+import nc.notus.states.UserRole;
+import nc.notus.states.WorkflowScenario;
+import nc.notus.workflow.DisconnectScenarioWorkflow;
 
 /**
  * Implements part of Installation Engineer dashboard
- * @author Vladimir Ermolenko
+ * @author Vladimir Ermolenko & Panchenko Dmytro
  */
 public class ProvisionEngineerTasksServlet extends HttpServlet {
    
+	private List<Task> newScenarioTasks;
+	private List<Task> disconnectScenarioTasks;
+	
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
@@ -51,8 +65,22 @@ public class ProvisionEngineerTasksServlet extends HttpServlet {
                 userID = userDAO.getUserByLogin(login).getId();
             }
             TaskDAO taskDAO = new TaskDAOImpl(dbManager);
-            List<Task> tasks = taskDAO.getTasksByID(startpage, numbOfRecords, userID);
-            request.setAttribute("tasks", tasks);
+            List<Task> tasks = taskDAO.getEngTasks(startpage, numbOfRecords, 
+					UserRole.PROVISION_ENGINEER.toInt());
+           // List<Task> tasks = taskDAO.getTasksByID(startpage, numbOfRecords, userID);
+            if(tasks.size() > 0) {
+            	
+            	sortTasksByScenario(tasks, dbManager);
+            	
+            	if (newScenarioTasks.size() > 0) {
+        			request.setAttribute("newScenarioTasks", newScenarioTasks);
+        		   }
+        		   if (disconnectScenarioTasks.size() > 0) {
+        			request.setAttribute("disconnectScenarioTasks",disconnectScenarioTasks);
+        		   }
+
+            }
+           // request.setAttribute("tasks", tasks);
             request.setAttribute("userid", userID);
             request.getRequestDispatcher("provisioningEngineer.jsp").forward(request, response);
         } finally {
@@ -60,6 +88,29 @@ public class ProvisionEngineerTasksServlet extends HttpServlet {
         }
     } 
 
+    private void sortTasksByScenario(List<Task> tasks, DBManager dbManager) {
+    	
+    	newScenarioTasks = new ArrayList<Task>();
+		disconnectScenarioTasks = new ArrayList<Task>();
+		
+		ServiceOrderDAOImpl soDAO = new ServiceOrderDAOImpl(dbManager);
+		ScenarioDAO scenarioDAO = new ScenarioDAOImpl(dbManager);
+
+		for (Task task : tasks) {
+			ServiceOrder order = soDAO.find(task.getServiceOrderID());
+
+			int scenarioID = order.getScenarioID();
+			Scenario scenario = scenarioDAO.find(scenarioID);
+
+			if (scenario.getScenario().equalsIgnoreCase(WorkflowScenario.NEW.toString())) {
+				newScenarioTasks.add(task);
+			} else  {
+				disconnectScenarioTasks.add(task);
+			} 
+		}
+		
+    }
+     
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
@@ -84,7 +135,32 @@ public class ProvisionEngineerTasksServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+    	if ("Delete Circuit from SI".equals(request.getParameter("action"))) {
+
+			DBManager dbManager = null;
+			try {
+				dbManager = new DBManager();
+				ServiceOrderDAOImpl orderDAO = new ServiceOrderDAOImpl(dbManager);
+				DisconnectScenarioWorkflow wf = null;
+				if(request.getParameter("d_orderid") == null 
+						| request.getParameter("d_taskid") == null) {
+					request.setAttribute("success", "Parameters not passes");
+				} else {
+				// getting ServiceOrder id and Task id
+				int orderID = Integer.parseInt(request.getParameter("d_orderid"));
+				int taskID = Integer.parseInt(request.getParameter("d_taskid"));
+
+				ServiceOrder order = orderDAO.find(orderID);
+				wf = new DisconnectScenarioWorkflow(order);
+				wf.removeCurcuitFromSI(taskID);
+
+				request.setAttribute("success", "Curcuit was removed!Order ");
+				processRequest(request, response);
+				}
+			} finally {
+				dbManager.close();
+			}
+    	}
     }
 
     /** 
