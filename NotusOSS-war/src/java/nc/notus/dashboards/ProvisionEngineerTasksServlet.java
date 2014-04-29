04b3f0ae25e638dae978d4c71087c6d0a6085124
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-04-29 17:41:31 +0300

    add getScenario and fixes, delete useless code

--- NotusOSS-war/src/java/nc/notus/dashboards/ProvisionEngineerTasksServlet.java
+++ NotusOSS-war/src/java/nc/notus/dashboards/ProvisionEngineerTasksServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import nc.notus.dao.OSSUserDAO;
 import nc.notus.dao.ScenarioDAO;
+import nc.notus.dao.ServiceOrderDAO;
 import nc.notus.dao.TaskDAO;
 import nc.notus.dao.impl.OSSUserDAOImpl;
 import nc.notus.dao.impl.ScenarioDAOImpl;
@@ -30,146 +31,146 @@ import nc.notus.states.WorkflowScenario;
 import nc.notus.workflow.DisconnectScenarioWorkflow;
 
 /**
- * Implements part of Installation Engineer dashboard
+ * Implements part of Provisioning Engineer dashboard
+ * 
  * @author Vladimir Ermolenko & Panchenko Dmytro
  */
 public class ProvisionEngineerTasksServlet extends HttpServlet {
-   
-	private List<Task> newScenarioTasks;
-	private List<Task> disconnectScenarioTasks;
-	
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        int startpage = 1;
-        int numbOfRecords = 10;
-        String login = "";
-        int userID = 0;
-        DBManager dbManager = new DBManager();
-        try {
-            if (request.getParameter("startpage") != null) {
-                startpage = Integer.parseInt(request.getParameter("startpage"));
-            }
-            if (request.getParameter("numbOfRecords") != null) {
-                numbOfRecords = Integer.parseInt(request.getParameter("numbOfRecords"));
-            }
-            OSSUserDAO userDAO = new OSSUserDAOImpl(dbManager);
-            if (userDAO.getUserByLogin(login) != null){
-                userID = userDAO.getUserByLogin(login).getId();
-            }
-            TaskDAO taskDAO = new TaskDAOImpl(dbManager);
-            List<Task> tasks = taskDAO.getEngTasks(startpage, numbOfRecords, 
-					UserRole.PROVISION_ENGINEER.toInt());
-           // List<Task> tasks = taskDAO.getTasksByID(startpage, numbOfRecords, userID);
-            if(tasks.size() > 0) {
-            	
-            	sortTasksByScenario(tasks, dbManager);
-            	
-            	if (newScenarioTasks.size() > 0) {
-        			request.setAttribute("newScenarioTasks", newScenarioTasks);
-        		   }
-        		   if (disconnectScenarioTasks.size() > 0) {
-        			request.setAttribute("disconnectScenarioTasks",disconnectScenarioTasks);
-        		   }
 
-            }
-           // request.setAttribute("tasks", tasks);
-            request.setAttribute("userid", userID);
-            request.getRequestDispatcher("provisioningEngineer.jsp").forward(request, response);
-        } finally {
-            dbManager.close();
-        }
-    } 
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+	 * methods.
+	 * 
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
 
-    private void sortTasksByScenario(List<Task> tasks, DBManager dbManager) {
-    	
-    	newScenarioTasks = new ArrayList<Task>();
-		disconnectScenarioTasks = new ArrayList<Task>();
-		
+		if (request.getParameter("taskid") != null) {
+			
+			int taskID = Integer.parseInt(request.getParameter("taskID"));
+			DBManager dbManager = null;
+			try {
+				dbManager = new DBManager();
+				TaskDAO taskDAO = new TaskDAOImpl(dbManager);
+
+				Task task = taskDAO.find(taskID);
+
+				String wfScenario = getTaskScenario(task, dbManager);
+				request.setAttribute("wfScenario", wfScenario);
+				request.setAttribute("task", task);
+				
+				request.getRequestDispatcher("provisioningEngineerWorkflow.jsp").forward(request, response);
+				
+			} finally {
+				dbManager.close();
+			}
+		}
+
+	}
+
+	private String getTaskScenario(Task task, DBManager dbManager) {
+
+		String wfScenario = null;
+
 		ServiceOrderDAOImpl soDAO = new ServiceOrderDAOImpl(dbManager);
 		ScenarioDAO scenarioDAO = new ScenarioDAOImpl(dbManager);
 
-		for (Task task : tasks) {
-			ServiceOrder order = soDAO.find(task.getServiceOrderID());
+		ServiceOrder order = soDAO.find(task.getServiceOrderID());
 
-			int scenarioID = order.getScenarioID();
-			Scenario scenario = scenarioDAO.find(scenarioID);
+		int scenarioID = order.getScenarioID();
+		Scenario scenario = scenarioDAO.find(scenarioID);
 
-			if (scenario.getScenario().equalsIgnoreCase(WorkflowScenario.NEW.toString())) {
-				newScenarioTasks.add(task);
-			} else  {
-				disconnectScenarioTasks.add(task);
-			} 
+		if (scenario.getScenario().equalsIgnoreCase(WorkflowScenario.NEW.toString())) {
+			wfScenario = "NEW";
+		} else if ((scenario.getScenario().equalsIgnoreCase(WorkflowScenario.DISCONNECT.toString()))) {
+			wfScenario = "DISCONNECT";
 		}
-		
-    }
-     
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    	if ("Delete Circuit from SI".equals(request.getParameter("action"))) {
+		return wfScenario;
+	}
+
+	// <editor-fold defaultstate="collapsed"
+	// desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 * 
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 * 
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		if ("Delete Circuit from SI".equals(request.getParameter("action"))) {
 
 			DBManager dbManager = null;
 			try {
 				dbManager = new DBManager();
 				ServiceOrderDAOImpl orderDAO = new ServiceOrderDAOImpl(dbManager);
+				
 				DisconnectScenarioWorkflow wf = null;
-				if(request.getParameter("d_orderid") == null 
+				if (request.getParameter("d_orderid") == null
 						| request.getParameter("d_taskid") == null) {
 					request.setAttribute("success", "Parameters not passes");
 				} else {
-				// getting ServiceOrder id and Task id
-				int orderID = Integer.parseInt(request.getParameter("d_orderid"));
-				int taskID = Integer.parseInt(request.getParameter("d_taskid"));
+					// getting ServiceOrder id and Task id
+					int orderID = Integer.parseInt(request.getParameter("d_orderid"));
+					int taskID = Integer.parseInt(request.getParameter("d_taskid"));
 
-				ServiceOrder order = orderDAO.find(orderID);
-				wf = new DisconnectScenarioWorkflow(order);
-				wf.removeCurcuitFromSI(taskID);
+					ServiceOrder order = orderDAO.find(orderID);
+					wf = new DisconnectScenarioWorkflow(order);
+					wf.removeCurcuitFromSI(taskID);
 
-				request.setAttribute("success", "Curcuit was removed!Order ");
-				processRequest(request, response);
+					request.setAttribute("success","Bill successfully sent!");
+					request.getRequestDispatcher("provisioningEngineerWorkflow.jsp").forward(request, response);
 				}
 			} finally {
 				dbManager.close();
 			}
-    	}
-    }
+		}
+	}
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	/**
+	 * Returns a short description of the servlet.
+	 * 
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 
 }
