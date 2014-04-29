651198b12443c1c5a5b4096e02b52d508d57f7a0
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-04-30 01:33:42 +0300

    add selecting "name" field

--- NotusOSS-war/src/java/nc/notus/dao/impl/TaskDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/TaskDAOImpl.java
@@ -2,6 +2,7 @@ package nc.notus.dao.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import nc.notus.dao.TaskDAO;
 import nc.notus.dbmanager.DBManager;
@@ -10,84 +11,80 @@ import nc.notus.dbmanager.Statement;
 import nc.notus.entity.Task;
 
 /**
- *
- * @author Igor Litvinenko & Vladimir Ermolenko
+ * 
+ * @author Igor Litvinenko & Vladimir Ermolenko & Panchenko Dmytro
  */
 public class TaskDAOImpl extends GenericDAOImpl<Task> implements TaskDAO {
 
-    public TaskDAOImpl(DBManager dbManager) {
-        super(dbManager);
-    }
-    
-    /**
-     * Method returns list of numberOfRecords choosen Engineer type tasks with paging;
-     * @param offset - offset from start position in paging
-     * @param numberOfRecords - quantity of records to fetch
-     * @param roleID - id of assigned role for tasks
-     * @return list of tasks
-     */
-    @Override
-    public List<Task> getEngTasks(int offset, int numberOfRecords, int roleID) {
-        String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
-                "SELECT t.id, t.serviceorderid, t.employeeid, t.roleid, t.taskstatusid " +
-                "FROM task t " +
-                "JOIN taskstatus ts ON t.taskstatusid = ts.id " +
-                "WHERE t.roleid = ? " +
-                "AND ts.status = 'Active' " +
-                "AND t.employeeid IS NULL " +
-                "ORDER BY t.id " +
-                ") a where ROWNUM <= ? )" +
-                "WHERE rnum  >= ?";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setInt(1, roleID);
-        statement.setInt(2, numberOfRecords);
-        statement.setInt(3, offset);
-        ResultIterator ri = statement.executeQuery();
-        List<Task> tasksEng = new ArrayList<Task>();
-        while (ri.next()){
-            Task task = new Task();
-            task.setId(ri.getInt("id"));
-            task.setServiceOrderID(ri.getInt("serviceorderid"));
-            task.setEmployeeID(ri.getInt("employeeid"));
-            task.setRoleID(ri.getInt("roleid"));
-            task.setTaskStatusID(ri.getInt("taskstatusid"));
-            tasksEng.add(task);
-        }
-        return tasksEng;
-    }
-    
-    /**
-     * Method returns list of numberOfRecords choosen userID tasks with paging;
-     * @param offset - offset from start position in paging
-     * @param numberOfRecords - quantity of records to fetch
-     * @param userID - user ID
-     * @return list of tasks
-     */
-    public List<Task> getTasksByID(int offset, int numberOfRecords, int userID) {
-        String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
-                "SELECT t.id, t.serviceorderid, t.employeeid, t.roleid, t.taskstatusid " +
-                "FROM task t " +
-                "JOIN taskstatus ts ON t.taskstatusid = ts.id " +
-                "WHERE ts.status = 'Active' " +
-                "AND t.employeeid = ? " +
-                "ORDER BY t.id " +
-                ") a where ROWNUM <= ? )" +
-                "WHERE rnum  >= ?";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setInt(1, userID);
-        statement.setInt(2, numberOfRecords);
-        statement.setInt(3, offset);
-        ResultIterator ri = statement.executeQuery();
-        List<Task> tasks = new ArrayList<Task>();
-        while (ri.next()){
-            Task task = new Task();
-            task.setId(ri.getInt("id"));
-            task.setServiceOrderID(ri.getInt("serviceorderid"));
-            task.setEmployeeID(ri.getInt("employeeid"));
-            task.setRoleID(ri.getInt("roleid"));
-            task.setTaskStatusID(ri.getInt("taskstatusid"));
-            tasks.add(task);
-        }
-        return tasks;
-    }
+	public TaskDAOImpl(DBManager dbManager) {
+		super(dbManager);
+	}
+
+	   @Override
+	    public List<Task> getEngTasks(int offset, int numberOfRecords, int roleID) {
+	        String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
+	                "SELECT t.id, t.serviceorderid, t.employeeid, t.roleid, t.taskstatusid, t.name " +
+	                "FROM task t " +
+	                "JOIN taskstatus ts ON t.taskstatusid = ts.id " +
+	                "WHERE t.roleid = ? " +
+	                "AND ts.status = 'Active' " +
+	                "AND t.employeeid IS NULL " +
+	                "ORDER BY t.id " +
+	                ") a where ROWNUM <= ? )" +
+	                "WHERE rnum  >= ?";
+	        Statement statement = dbManager.prepareStatement(query);
+	        statement.setInt(1, roleID);
+	        statement.setInt(2, numberOfRecords);
+	        statement.setInt(3, offset);
+	        ResultIterator ri = statement.executeQuery();
+	        List<Task> tasksEng = new ArrayList<Task>();
+	        while (ri.next()){
+	            Task task = new Task();
+	            task.setId(ri.getInt("id"));
+	            task.setServiceOrderID(ri.getInt("serviceorderid"));
+	            task.setEmployeeID(ri.getInt("employeeid"));
+	            task.setRoleID(ri.getInt("roleid"));
+	            task.setTaskStatusID(ri.getInt("taskstatusid"));
+	            task.setName(ri.getString("name"));
+	            tasksEng.add(task);
+	        }
+	        return tasksEng;
+	    }
+	    
+	    /**
+	     * Method returns list of numberOfRecords choosen userID tasks with paging;
+	     * @param offset - offset from start position in paging
+	     * @param numberOfRecords - quantity of records to fetch
+	     * @param userID - user ID
+	     * @return list of tasks
+	     */
+	    public List<Task> getTasksByID(int offset, int numberOfRecords, int userID) {
+	        String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
+	                "SELECT t.id, t.serviceorderid, t.employeeid, t.roleid, t.taskstatusid, t.name " +
+	                "FROM task t " +
+	                "JOIN taskstatus ts ON t.taskstatusid = ts.id " +
+	                "WHERE ts.status = 'Active' " +
+	                "AND t.employeeid = ? " +
+	                "ORDER BY t.id " +
+	                ") a where ROWNUM <= ? )" +
+	                "WHERE rnum  >= ?";
+	        Statement statement = dbManager.prepareStatement(query);
+	        statement.setInt(1, userID);
+	        statement.setInt(2, numberOfRecords);
+	        statement.setInt(3, offset);
+	        ResultIterator ri = statement.executeQuery();
+	        List<Task> tasks = new ArrayList<Task>();
+	        while (ri.next()){
+	            Task task = new Task();
+	            task.setId(ri.getInt("id"));
+	            task.setServiceOrderID(ri.getInt("serviceorderid"));
+	            task.setEmployeeID(ri.getInt("employeeid"));
+	            task.setRoleID(ri.getInt("roleid"));
+	            task.setTaskStatusID(ri.getInt("taskstatusid"));
+	            task.setName(ri.getString("name"));
+	            tasks.add(task);
+	        }
+	        return tasks;
+	    }
+
 }
