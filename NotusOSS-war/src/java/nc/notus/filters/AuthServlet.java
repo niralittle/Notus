f40a07d90945803aadbd424d7ba5a88332b373be
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-05-01 13:04:13 +0300

    add checking if user blocked

--- NotusOSS-war/src/java/nc/notus/filters/AuthServlet.java
+++ NotusOSS-war/src/java/nc/notus/filters/AuthServlet.java
@@ -6,79 +6,94 @@
 package nc.notus.filters;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import nc.notus.dao.OSSUserDAO;
+import nc.notus.dao.impl.OSSUserDAOImpl;
+import nc.notus.dbmanager.DBManager;
+
 /**
+ * Check if user blocked. If yes - then redirect to error-page.             
+ *                 
+ * @author Pacnhenko Dmytro
  *
- * @author Vladimir Ermolenko
  */
 public class AuthServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AuthServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AuthServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            */
-        } finally { 
-            out.close();
-        }
-    } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+	private static final long serialVersionUID = 1L;
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+
+	private static final String LOGIN_ERROR_PAGE = "loginError.jsp";
+	
+	protected void processRequest(HttpServletRequest request,
+                HttpServletResponse response)
+                throws ServletException, IOException {
+		
+		String username = request.getParameter("j_username");
+		String password = request.getParameter("j_password");
+		
+		OSSUserDAO userDAO = null;
+		DBManager dbManager = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			dbManager = new DBManager();
+			userDAO = new OSSUserDAOImpl(dbManager);
+			if(userDAO.isBlocked(username)) {
+				sb.append("User with specified login blocked!");
+				request.setAttribute("errMessage", sb.toString());
+				redirect(request, response, LOGIN_ERROR_PAGE);
+			} 
+		} finally {
+			dbManager.close();
+		}
+		
+		response.sendRedirect("j_security_check?j_username=" 
+				+ username + "&j_password="+password);
     }
+	
+	/**
+	 * Redirect to passes page.
+	 * 
+	 * @param request
+	 * @param response
+	 * @param page
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void redirect(HttpServletRequest request,
+			HttpServletResponse response, String page) throws ServletException,
+			IOException {
+		RequestDispatcher view = request.getRequestDispatcher(page);
+		view.forward(request, response);
+		return;
+	}
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+                        throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+                        throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	
+	@Override
+	public String getServletInfo() {
+		return "LogoutServlet invalidates current session " +
+                        "and logs the user out.";
+	}
 
 }
