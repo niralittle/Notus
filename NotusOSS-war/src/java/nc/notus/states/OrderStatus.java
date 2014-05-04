7734d77ca183e7bc86e5ea516dd0e0c612885a12
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-05-05 01:49:11 +0300

    add toInt method to orderStatus

--- NotusOSS-war/src/java/nc/notus/states/OrderStatus.java
+++ NotusOSS-war/src/java/nc/notus/states/OrderStatus.java
@@ -7,17 +7,23 @@ package nc.notus.states;
  * @author Igor Litvinenko
  */
 public enum OrderStatus {
-    ENTERING("Entering"),
-    CANCELLED("Cancelled"),
-    PROCESSING("Processing"),
-    COMPLETED("Completed");
+    ENTERING("Entering",1),
+    CANCELLED("Cancelled",2),
+    PROCESSING("Processing",3),
+    COMPLETED("Completed",4);
 
     private final String status;
+    private final int statusId;
 
-    OrderStatus(String status) {
+    OrderStatus(String status, int statusId) {
         this.status = status;
+        this.statusId = statusId;
     }
 
+    public int toInt() {
+    	return statusId;
+    }
+    
     @Override
     public String toString() {
         return status;
