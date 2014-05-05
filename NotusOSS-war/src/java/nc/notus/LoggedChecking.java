dc5e2111adb2560db6518baae1e254ac17fa2e1f
Vladimir Ermolenko <vlader2@yahoo.com>
2014-05-05 09:06:39 +0300

    done possibility to add new service while user is logged in system

--- NotusOSS-war/src/java/nc/notus/LoggedChecking.java
+++ NotusOSS-war/src/java/nc/notus/LoggedChecking.java
@@ -6,18 +6,44 @@
 package nc.notus;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.Calendar;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import nc.notus.dao.OSSUserDAO;
+import nc.notus.dao.ScenarioDAO;
+import nc.notus.dao.ServiceOrderDAO;
+import nc.notus.dao.ServiceOrderStatusDAO;
+import nc.notus.dao.TaskDAO;
+import nc.notus.dao.impl.OSSUserDAOImpl;
+import nc.notus.dao.impl.ScenarioDAOImpl;
+import nc.notus.dao.impl.ServiceOrderDAOImpl;
+import nc.notus.dao.impl.ServiceOrderStatusDAOImpl;
+import nc.notus.dao.impl.TaskDAOImpl;
+import nc.notus.dbmanager.DBManager;
+import nc.notus.entity.OSSUser;
+import nc.notus.entity.ServiceOrder;
+import nc.notus.states.OrderStatus;
+import nc.notus.states.WorkflowScenario;
+import nc.notus.workflow.NewScenarioWorkflow;
+import nc.notus.workflow.Workflow;
 
 /**
  *
  * @author Vladimir Ermolenko
  */
 public class LoggedChecking extends HttpServlet {
-   
+
+    String login;
+    int catalogID;
+    String serviceLocation;
+    TaskDAO taskDAO = null;
+    OSSUser user = null;
+    ServiceOrder newOrder;
+    Integer userID = null;
+
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
@@ -28,22 +54,72 @@ public class LoggedChecking extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        String login;
+        DBManager dbManager = new DBManager();
+        
         try {
+            taskDAO = new TaskDAOImpl(dbManager);
+            OSSUserDAO userDAO = new OSSUserDAOImpl(dbManager);
+
+            // if user already logged in system
             if (request.getUserPrincipal() != null) {
                 login = request.getUserPrincipal().getName();
-                out.println(login);
             }
             else {
+                //if user not logged but registered user
                 response.sendRedirect("CustomerUser");
                 return;
             }
+            if (request.getParameter("serviceCatalogID") != null) {
+                catalogID = Integer.parseInt(request.getParameter("serviceCatalogID"));
+            }
+            if (request.getParameter("serviceLocationID") != null) {
+                serviceLocation = request.getParameter("serviceLocationID");
+            }
+            serviceLocation = java.net.URLDecoder.decode(serviceLocation,"UTF-8");
+
+            if (userDAO.getUserByLogin(login) != null){
+                user = userDAO.getUserByLogin(login);
+            }
+            
+            userID = user.getId();
+            // create new Service Order
+            newOrder = createOrder(dbManager, userID);
+            dbManager.commit();
+            // proceed Service Order
+            Workflow wf = new NewScenarioWorkflow(newOrder);
+            wf.proceedOrder();
+            response.sendRedirect("CustomerUser");
+            return;
 
         } finally { 
-            out.close();
+            dbManager.close();
         }
-    } 
+    }
+
+    private ServiceOrder createOrder(DBManager dbManager, int userID) {
+
+		ServiceOrderStatusDAO statusDAO = new ServiceOrderStatusDAOImpl(dbManager);
+		ScenarioDAO scenarioDAO = new ScenarioDAOImpl(dbManager);
+		ServiceOrderDAO orderDAO = new ServiceOrderDAOImpl(dbManager);
+
+		// create new order with status ENTERING
+		ServiceOrder so = new ServiceOrder();
+		int orderStatusID = statusDAO.getServiceOrderStatusID(OrderStatus.ENTERING);
+			so.setServiceOrderStatusID(orderStatusID);
+			so.setScenarioID(scenarioDAO.getScenarioID(WorkflowScenario.NEW));
+			so.setServiceCatalogID(catalogID);
+			so.setServiceInstanceID(null);
+			so.setServiceLocation(serviceLocation);
+			so.setUserID(userID);
+
+		Calendar cal = java.util.Calendar.getInstance();
+		Date date = new Date(cal.getTimeInMillis());
+			so.setServiceOrderDate(date);
+
+		int orderID = (Integer) orderDAO.add(so);
+		so.setId(orderID);
+		return so;
+	}
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
