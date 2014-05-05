9d40d67a4526f9f1f29680ee19cd03a336d78dc8
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-05-05 10:13:05 +0300

    Add redirecting logic

--- NotusOSS-war/src/java/nc/notus/filters/RedirectingServlet.java
+++ NotusOSS-war/src/java/nc/notus/filters/RedirectingServlet.java
@@ -1,84 +1,107 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package nc.notus.filters;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
+ * Redirect engineers to personal tasks page.         
+ *                 
+ * @author Pacnhenko Dmytro
  *
- * @author Vladimir Ermolenko
  */
 public class RedirectingServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RedirectingServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RedirectingServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            */
-        } finally { 
-            out.close();
-        }
-    } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+	private static final long serialVersionUID = 1L;
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+	
+	protected void processRequest(HttpServletRequest request,
+                HttpServletResponse response)
+                throws ServletException, IOException {
+		
+		if(isAuthorized(request) == false) {
+			redirect(request, response, "index.jsp");
+		} else if (isEngineer(request)) {
+			redirect(request, response, "TasksAssignment?type=personal");
+		} else if(isAdministator(request)) {
+			redirect(request, response, "adminDashboard.jsp");
+		} else if(isCustomerUser(request)) {
+			redirect(request, response, "index.jsp");
+		}
     }
+	
+	private boolean isEngineer(HttpServletRequest request) {
+		if(request.isUserInRole("SUPPORT_ENGINEER") 
+				|| request.isUserInRole("PROVISION_ENGINEER")
+				|| request.isUserInRole("INSTALLATION_ENGINEER")) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isCustomerUser(HttpServletRequest request) {
+		if(request.isUserInRole("CUSTOMER_USER")) {
+			return true;
+		}
+		return false;
+	}
+	private boolean isAuthorized(HttpServletRequest request) {
+		if(request.getUserPrincipal() != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isAdministator(HttpServletRequest request) {
+		if(request.isUserInRole("ADMINISTRATOR")) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Redirect to passes page.
+	 * 
+	 * @param request
+	 * @param response
+	 * @param page
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void redirect(HttpServletRequest request,
+			HttpServletResponse response, String page) throws ServletException,
+			IOException {
+		RequestDispatcher view = request.getRequestDispatcher(page);
+		view.forward(request, response);
+		return;
+	}
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+                        throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+                        throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	
+	@Override
+	public String getServletInfo() {
+		return "LogoutServlet invalidates current session " +
+                        "and logs the user out.";
+	}
 
 }
