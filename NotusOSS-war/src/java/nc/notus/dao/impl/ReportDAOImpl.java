a332d6f5ce4b961b4e8bd718f9e457e110ebe022
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-05-06 17:07:04 +0300

    add logging, some fixes

--- NotusOSS-war/src/java/nc/notus/dao/impl/ReportDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/ReportDAOImpl.java
@@ -5,8 +5,12 @@ import java.io.Writer;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+
 import nc.notus.dao.ReportDAO;
 import nc.notus.dbmanager.DBManager;
+import nc.notus.dbmanager.DBManagerException;
 import nc.notus.dbmanager.ResultIterator;
 import nc.notus.dbmanager.Statement;
 import nc.notus.entity.MostProfitableRouterReportData;
@@ -20,6 +24,8 @@ import nc.notus.entity.ServiceOrderReportData;
  */
 public class ReportDAOImpl implements ReportDAO {
 
+	private static Logger logger = Logger.getLogger(ReportDAOImpl.class.getName());
+	
     private DBManager dbManager;
 
     public ReportDAOImpl(DBManager dbManager) {
@@ -34,7 +40,12 @@ public class ReportDAOImpl implements ReportDAO {
      */
     @Override
     public MostProfitableRouterReportData getMostProfitableRouter() {
-        String query = "SELECT d.id, d.name, d.portquantity, sum(sc.price) total " +
+        
+    	MostProfitableRouterReportData  data = null;
+    	Statement statement = null;
+    	ResultIterator ri = null;
+    	
+    	String query = "SELECT d.id, d.name, d.portquantity, sum(sc.price) total " +
                 "FROM serviceorder so " +
                 "LEFT JOIN servicecatalog sc ON so.servicecatalogid = sc.id " +
                 "LEFT JOIN serviceinstance si ON so.serviceinstanceid = si.id " +
@@ -44,18 +55,22 @@ public class ReportDAOImpl implements ReportDAO {
                 "WHERE sis.status = 'Active' " +
                 "GROUP BY d.id, d.name, d.portquantity " +
                 "ORDER BY total DESC";
-        Statement statement = dbManager.prepareStatement(query);
-        ResultIterator ri = statement.executeQuery();
-        if (ri.next()) {
-            MostProfitableRouterReportData data = new MostProfitableRouterReportData();
-            data.setId(ri.getInt("id"));
-            data.setName(ri.getString("name"));
-            data.setPortQuantity(ri.getInt("portquantity"));
-            data.setProfit(ri.getInt("total"));
-            return data;
-        } else {
-            return null;
-        }
+		try {
+			statement = dbManager.prepareStatement(query);
+			ri = statement.executeQuery();
+			if (ri.next()) {
+				data = new MostProfitableRouterReportData();
+				data.setId(ri.getInt("id"));
+				data.setName(ri.getString("name"));
+				data.setPortQuantity(ri.getInt("portquantity"));
+				data.setProfit(ri.getInt("total"));
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
+		return data;
     }
 
     /**
@@ -69,7 +84,12 @@ public class ReportDAOImpl implements ReportDAO {
     @Override
     public List<ServiceOrderReportData> getNewServiceOrders(Date startDate,
             Date finishDate, int offset, int numberOfRecords) {
-        String query = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM ( " +
+        
+    	Statement statement = null;
+    	ResultIterator ri = null;
+    	List<ServiceOrderReportData> orders = null;
+    	
+    	String query = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM ( " +
                 "SELECT so.id, so.serviceorderdate, so.servicelocation, " +
                 "st.service, sc.price, pl.name, pl.location " +
                 "FROM serviceorder so " +
@@ -82,24 +102,32 @@ public class ReportDAOImpl implements ReportDAO {
                 "ORDER BY so.serviceorderdate ASC " +
                 ") a where ROWNUM <= ? ) " +
                 "WHERE rnum  > ?";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setDate(1, startDate);
-        statement.setDate(2, finishDate);
-        statement.setInt(3, offset + numberOfRecords);
-        statement.setInt(4, offset);
-        ResultIterator ri = statement.executeQuery();
-        List<ServiceOrderReportData> orders = new ArrayList<ServiceOrderReportData>();
-        while (ri.next()) {
-            ServiceOrderReportData serviceOrderData = new ServiceOrderReportData();
-            serviceOrderData.setId(ri.getInt("id"));
-            serviceOrderData.setDate(ri.getDate("serviceorderdate").toString());
-            serviceOrderData.setServiceLocation(ri.getString("servicelocation"));
-            serviceOrderData.setServiceName(ri.getString("service"));
-            serviceOrderData.setPrice(ri.getInt("price"));
-            serviceOrderData.setProviderLocationName(ri.getString("name"));
-            serviceOrderData.setProviderLocation(ri.getString("location"));
-            orders.add(serviceOrderData);
-        }
+		try {
+			statement = dbManager.prepareStatement(query);
+			statement.setDate(1, startDate);
+			statement.setDate(2, finishDate);
+			statement.setInt(3, offset + numberOfRecords);
+			statement.setInt(4, offset);
+			
+			ri = statement.executeQuery();
+			orders = new ArrayList<ServiceOrderReportData>();
+			
+			while (ri.next()) {
+				ServiceOrderReportData serviceOrderData = new ServiceOrderReportData();
+				serviceOrderData.setId(ri.getInt("id"));
+				serviceOrderData.setDate(ri.getDate("serviceorderdate").toString());
+				serviceOrderData.setServiceLocation(ri.getString("servicelocation"));
+				serviceOrderData.setServiceName(ri.getString("service"));
+				serviceOrderData.setPrice(ri.getInt("price"));
+				serviceOrderData.setProviderLocationName(ri.getString("name"));
+				serviceOrderData.setProviderLocation(ri.getString("location"));
+				orders.add(serviceOrderData);
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
         return orders;
     }
 
@@ -114,7 +142,12 @@ public class ReportDAOImpl implements ReportDAO {
     @Override
     public List<ServiceOrderReportData> getDisconnectServiceOrders(Date startDate,
             Date finishDate, int offset, int numberOfRecords) {
-        String query = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM ( " +
+    	
+    	Statement statement = null;
+    	ResultIterator ri = null;
+    	List<ServiceOrderReportData> orders = null;
+    	
+    	String query = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM ( " +
                 "SELECT so.id, so.serviceorderdate, so.servicelocation, " +
                 "st.service, sc.price, pl.name, pl.location " +
                 "FROM serviceorder so " +
@@ -127,24 +160,31 @@ public class ReportDAOImpl implements ReportDAO {
                 "ORDER BY so.serviceorderdate ASC " +
                 ") a where ROWNUM <= ? ) " +
                 "WHERE rnum  > ?";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setDate(1, startDate);
-        statement.setDate(2, finishDate);
-        statement.setInt(3, offset + numberOfRecords);
-        statement.setInt(4, offset);
-        ResultIterator ri = statement.executeQuery();
-        List<ServiceOrderReportData> orders = new ArrayList<ServiceOrderReportData>();
-        while (ri.next()) {
-            ServiceOrderReportData serviceOrderData = new ServiceOrderReportData();
-            serviceOrderData.setId(ri.getInt("id"));
-            serviceOrderData.setDate(ri.getDate("serviceorderdate").toString());
-            serviceOrderData.setServiceLocation(ri.getString("servicelocation"));
-            serviceOrderData.setServiceName(ri.getString("service"));
-            serviceOrderData.setPrice(ri.getInt("price"));
-            serviceOrderData.setProviderLocationName(ri.getString("name"));
-            serviceOrderData.setProviderLocation(ri.getString("location"));
-            orders.add(serviceOrderData);
-        }
+        try{
+			statement = dbManager.prepareStatement(query);
+			statement.setDate(1, startDate);
+			statement.setDate(2, finishDate);
+			statement.setInt(3, offset + numberOfRecords);
+			statement.setInt(4, offset);
+
+			ri = statement.executeQuery();
+			orders = new ArrayList<ServiceOrderReportData>();
+			while (ri.next()) {
+				ServiceOrderReportData serviceOrderData = new ServiceOrderReportData();
+				serviceOrderData.setId(ri.getInt("id"));
+				serviceOrderData.setDate(ri.getDate("serviceorderdate").toString());
+				serviceOrderData.setServiceLocation(ri.getString("servicelocation"));
+				serviceOrderData.setServiceName(ri.getString("service"));
+				serviceOrderData.setPrice(ri.getInt("price"));
+				serviceOrderData.setProviderLocationName(ri.getString("name"));
+				serviceOrderData.setProviderLocation(ri.getString("location"));
+				orders.add(serviceOrderData);
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
         return orders;
     }
 
@@ -160,7 +200,10 @@ public class ReportDAOImpl implements ReportDAO {
     public List<RoutersUtilizationCapacity> getRoutersUtilizationCapacityData(
             int offset, int numberOfRecords) {
 
-
+    	Statement statement = null;
+    	ResultIterator ri = null;
+    	List<RoutersUtilizationCapacity> routersUtilizationCapacity = null;
+    	
         String query = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM ( " +
                 "SELECT d.id, d.name, d.portquantity," +
                 "COUNT(p.portnumber) / d.portquantity * 100 AS utilization " +
@@ -169,24 +212,30 @@ public class ReportDAOImpl implements ReportDAO {
                 "WHERE p.portstatus = '1' GROUP BY d.id, d.name, d.portquantity" +
                 ") a where ROWNUM <= ? ) " +
                 "WHERE rnum  > ?";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setInt(1, offset + numberOfRecords);
-        statement.setInt(2, offset);
-        ResultIterator ri = statement.executeQuery();
-        List<RoutersUtilizationCapacity> routersUtilizationCapacity =
-                new ArrayList<RoutersUtilizationCapacity>();
-        while (ri.next()) {
-            RoutersUtilizationCapacity routUtCap = new RoutersUtilizationCapacity();
-            routUtCap.setDeviceId(ri.getInt("id"));
-            routUtCap.setDeviceName(ri.getString("name"));
-            routUtCap.setCapacity(ri.getInt("portquantity"));
-            routUtCap.setUtilization(ri.getFloat("utilization"));
-            routersUtilizationCapacity.add(routUtCap);
-        }
+		try {
+			statement = dbManager.prepareStatement(query);
+			statement.setInt(1, offset + numberOfRecords);
+			statement.setInt(2, offset);
+			ri = statement.executeQuery();
+			routersUtilizationCapacity = new ArrayList<RoutersUtilizationCapacity>();
+			while (ri.next()) {
+				RoutersUtilizationCapacity routUtCap = new RoutersUtilizationCapacity();
+				routUtCap.setDeviceId(ri.getInt("id"));
+				routUtCap.setDeviceName(ri.getString("name"));
+				routUtCap.setCapacity(ri.getInt("portquantity"));
+				routUtCap.setUtilization(ri.getFloat("utilization"));
+				routersUtilizationCapacity.add(routUtCap);
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
         return routersUtilizationCapacity;
     }
 
     ////////////////////////////////////////MODIFY\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+   
     /**
      * Method that returns list of objects for profitability by month report
      * @param startDate - start of period
@@ -195,16 +244,27 @@ public class ReportDAOImpl implements ReportDAO {
      */
     @Override
     public List<ProfitInMonth> getProfitByMonth(Date month) {
+        Statement statement = null;
+        ResultIterator ri = null;
+        List<ProfitInMonth> profitByMonths = null;
+        
         String query = "";
-        Statement statement = dbManager.prepareStatement(query);
-        ResultIterator ri = statement.executeQuery();
-        List<ProfitInMonth> profitByMonths = new ArrayList<ProfitInMonth>();
-        while (ri.next()) {
-            ProfitInMonth profInMonth = new ProfitInMonth();
-            profInMonth.setProfit(ri.getInt("profit"));
-            profInMonth.setMonth(ri.getString("months"));
-            profitByMonths.add(profInMonth);
-        }
+        
+		try {
+			statement = dbManager.prepareStatement(query);
+			ri = statement.executeQuery();
+			profitByMonths = new ArrayList<ProfitInMonth>();
+			while (ri.next()) {
+				ProfitInMonth profInMonth = new ProfitInMonth();
+				profInMonth.setProfit(ri.getInt("profit"));
+				profInMonth.setMonth(ri.getString("months"));
+				profitByMonths.add(profInMonth);
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
         return profitByMonths;
     }
 
@@ -232,7 +292,12 @@ public class ReportDAOImpl implements ReportDAO {
     @Override
     public void getNewServiceOrders(Writer storage, String columnSeparator,
             Date startDate, Date finishDate) throws IOException {
-        String query = "SELECT so.id, so.serviceorderdate, so.servicelocation, " +
+       
+    	Statement statement = null;
+        ResultIterator ri = null;
+        List<ProfitInMonth> orders = null;
+         
+    	String query = "SELECT so.id, so.serviceorderdate, so.servicelocation, " +
                 "st.service, sc.price, pl.name, pl.location " +
                 "FROM serviceorder so " +
                 "LEFT JOIN scenario s ON so.scenarioid = s.id " +
@@ -242,26 +307,33 @@ public class ReportDAOImpl implements ReportDAO {
                 "WHERE so.serviceorderdate BETWEEN ? AND ? " +
                 "AND s.scenario = 'New' " +
                 "ORDER BY so.serviceorderdate ASC ";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setDate(1, startDate);
-        statement.setDate(2, finishDate);
-        ResultIterator ri = statement.executeQuery();
-        while (ri.next()) {
-            storage.write(String.valueOf(ri.getInt("id")));
-            storage.write(columnSeparator);
-            storage.write(ri.getDate("serviceorderdate").toString());
-            storage.write(columnSeparator);
-            storage.write(ri.getString("servicelocation"));
-            storage.write(columnSeparator);
-            storage.write(ri.getString("service"));
-            storage.write(columnSeparator);
-            storage.write(String.valueOf(ri.getInt("price")));
-            storage.write(columnSeparator);
-            storage.write(ri.getString("name"));
-            storage.write(columnSeparator);
-            storage.write(ri.getString("location"));
-            storage.write("\n");
-        }
+		try {
+			statement = dbManager.prepareStatement(query);
+			statement.setDate(1, startDate);
+			statement.setDate(2, finishDate);
+			ri = statement.executeQuery();
+			while (ri.next()) {
+				storage.write(String.valueOf(ri.getInt("id")));
+				storage.write(columnSeparator);
+				storage.write(ri.getDate("serviceorderdate").toString());
+				storage.write(columnSeparator);
+				storage.write(ri.getString("servicelocation"));
+				storage.write(columnSeparator);
+				storage.write(ri.getString("service"));
+				storage.write(columnSeparator);
+				storage.write(String.valueOf(ri.getInt("price")));
+				storage.write(columnSeparator);
+				storage.write(ri.getString("name"));
+				storage.write(columnSeparator);
+				storage.write(ri.getString("location"));
+				storage.write("\n");
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
+    	//return orders;
     }
 
     /**
