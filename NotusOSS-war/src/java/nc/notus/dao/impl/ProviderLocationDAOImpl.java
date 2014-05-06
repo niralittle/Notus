ef65f732b37404d19e8b565e4d1be663e4f3ac5f
Dmytro <panchenko.dmytro.kyiv@gmail.com>
2014-05-06 16:51:30 +0300

    add logging

--- NotusOSS-war/src/java/nc/notus/dao/impl/ProviderLocationDAOImpl.java
+++ NotusOSS-war/src/java/nc/notus/dao/impl/ProviderLocationDAOImpl.java
@@ -2,8 +2,12 @@ package nc.notus.dao.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+
 import nc.notus.dao.ProviderLocationDAO;
 import nc.notus.dbmanager.DBManager;
+import nc.notus.dbmanager.DBManagerException;
 import nc.notus.dbmanager.ResultIterator;
 import nc.notus.dbmanager.Statement;
 import nc.notus.entity.ProviderLocation;
@@ -14,9 +18,12 @@ import nc.notus.entity.ProviderLocation;
  */
 public class ProviderLocationDAOImpl extends GenericDAOImpl<ProviderLocation> implements ProviderLocationDAO {
 
+	private static Logger logger = Logger.getLogger(ProviderLocationDAOImpl.class.getName());
+	
     public ProviderLocationDAOImpl(DBManager dbManager) {
         super(dbManager);
     }
+    
     /**
      * Method returns list of all (numberOfRecords) provided locations with paging
      * @param offset - offset from start position in paging
@@ -25,22 +32,34 @@ public class ProviderLocationDAOImpl extends GenericDAOImpl<ProviderLocation> im
      */
     @Override
     public List<ProviderLocation> getProviderLocations(int offset, int numberOfRecords) {
-        String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
+    	Statement statement = null;
+    	ResultIterator ri = null;
+    	List<ProviderLocation> providerLocations = null;
+    	
+    	String query  = "SELECT * FROM ( SELECT a.*, ROWNUM rnum FROM (" +
                 "SELECT pl.id, pl.name, pl.location FROM providerlocation pl" +
                 ") a where ROWNUM <= ? )" +
                 "WHERE rnum  >= ?";
-        Statement statement = dbManager.prepareStatement(query);
-        statement.setInt(1, numberOfRecords);
-        statement.setInt(2, offset);
-        ResultIterator ri = statement.executeQuery();
-        List<ProviderLocation> providerLocations = new ArrayList<ProviderLocation>();
-        while (ri.next()){
-            ProviderLocation provLoc = new ProviderLocation();
-            provLoc.setId(ri.getInt("id"));
-            provLoc.setName(ri.getString("name"));
-            provLoc.setLocation(ri.getString("location"));
-            providerLocations.add(provLoc);
-        }
+		try {
+			statement = dbManager.prepareStatement(query);
+			statement.setInt(1, numberOfRecords);
+			statement.setInt(2, offset);
+
+			ri = statement.executeQuery();
+			providerLocations = new ArrayList<ProviderLocation>();
+
+			while (ri.next()) {
+				ProviderLocation provLoc = new ProviderLocation();
+				provLoc.setId(ri.getInt("id"));
+				provLoc.setName(ri.getString("name"));
+				provLoc.setLocation(ri.getString("location"));
+				providerLocations.add(provLoc);
+			}
+		} catch (DBManagerException exc) {
+			logger.error(exc.getMessage(), exc);
+		} finally {
+			statement.close();
+		}
         return providerLocations;
     }
 
